a76207b72a1090af20d99f46e8e402d9c2bb47f3
syd <sydneymainga6@gmail.com>
2019-04-04 17:12:36 +0300

    added splashscreen

--- app/src/main/java/com/example/imarisha/MainActivity.java
+++ app/src/main/java/com/example/imarisha/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.imarisha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.mrgames13.jimdo.splashscreen.App.SplashScreenBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SplashScreenBuilder.getInstance(this)
+                .setVideo(R.raw.splash_animation)
+                .setImage(R.drawable.app_icon)
+                .skipImage(true)
+                .show();
+
+        /**Button restart_app = findViewById(R.id.restart_app);
+        restart_app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Restart app
+                Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });**/
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == SplashScreenBuilder.SPLASH_SCREEN_FINISHED) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this, "SplashScreen finished", Toast.LENGTH_SHORT).show();
+            } else if(resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "SplashScreen finished, but canceled", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
     }
 }
