1533dc315851d1555096f819c6777823c6f38e3e
chesirekiptui <kiptui.chesire@students.jkuat.ac.ke>
2019-04-05 07:06:53 +0300

    health leader section

--- app/src/main/java/com/example/imarisha/Login_health.java
+++ app/src/main/java/com/example/imarisha/Login_health.java
@@ -3,6 +3,7 @@ package com.example.imarisha;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -333,6 +334,8 @@ public class Login_health extends AppCompatActivity implements LoaderCallbacks<C
             showProgress(false);
 
             if (success) {
+                Intent i = new Intent(getApplicationContext(),health_leader_activity.class);
+                startActivity(i);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
